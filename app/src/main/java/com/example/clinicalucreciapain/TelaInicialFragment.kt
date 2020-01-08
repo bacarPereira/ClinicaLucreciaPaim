970f933ccc8a944f428778242475e9b0fc1b35e1
BacarPereira <bacarpereira19@hotmail.com>
2020-01-08 23:17:23 +0100

    cONSTRUÃO HOME

--- app/src/main/java/com/example/clinicalucreciapain/TelaInicialFragment.kt
+++ app/src/main/java/com/example/clinicalucreciapain/TelaInicialFragment.kt
@@ -2,23 +2,26 @@ package com.example.clinicalucreciapain
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_tela_inicial.view.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class TelaInicialFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tela_inicial, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_tela_inicial, container, false)
+        onClick(view)
+        return view
     }
 
-
+    private fun onClick(view: View) {
+        view.btnLogin.setOnClickListener{
+            findNavController().navigate(TelaInicialFragmentDirections.actionTelaInicialFragmentToInicialSessaoPacienteFragment2())
+        }
+    }
 }
